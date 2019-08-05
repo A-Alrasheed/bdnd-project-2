eb148177bf6870d8ef336307c795bc13392f0a1f
Alrasheed A <alrasheed-a@outlook.com>
2019-08-06 02:26:27 +0300

    add properties to class Block

--- Block.js
+++ Block.js
@@ -3,9 +3,14 @@
 |  ===============================================*/
 
 class Block {
-	constructor(data){
-		// Add your Block properties
-		// Example: this.hash = "";
+	constructor(data) {
+		{
+			this.hash = "",
+			this.height = 0,
+			this.body = data,
+			this.time = 0,
+			this.prelockhash = ""
+		}
 	}
 }
 
